f030b624fb0d3c380ccaba7951b7427e4e0c71a2
martin jakubik <marcin.jakubik@sap.com>
2020-12-04 15:58:16 +0100

    whitespace

--- server.js
+++ server.js
@@ -7,44 +7,44 @@ var sBaseDirectory = ".";
 var port = 8080;
 
 oHttp.createServer(function (oRequest, oResponse) {
-   try {
-     var oRequestUrl = oUrl.parse(oRequest.url);
+  try {
+    var oRequestUrl = oUrl.parse(oRequest.url);
 
-     var sPath = oRequestUrl.pathname;
+    var sPath = oRequestUrl.pathname;
 
-     // need to use oPath.normalize so people can't access directories underneath sBaseDirectory
-     var sFSPath = sBaseDirectory + oPath.normalize(sPath);
-     console.log("path: \"" + sFSPath + "\"");
+    // need to use oPath.normalize so people can't access directories underneath sBaseDirectory
+    var sFSPath = sBaseDirectory + oPath.normalize(sPath);
+    console.log("path: \"" + sFSPath + "\"");
 
-     var sContentType = "text/plain";
+    var sContentType = "text/plain";
+    
+    if (sFSPath.includes("/css/")) {
+        sContentType = "text/css";
+    } else if (sFSPath.includes("/html/")) {
+        sContentType = "text/html";
+    } else if (sFSPath.includes("/js/")) {
+        sContentType = "application/javascript";
+    }
 
-     if (sFSPath.includes("/css/")) {
-         sContentType = "text/css";
-     } else if (sFSPath.includes("/html/")) {
-         sContentType = "text/html";
-     } else if (sFSPath.includes("/js/")) {
-         sContentType = "application/javascript";
-     }
+    var oHeaders =  {
+       "Content-Type": sContentType
+    };
 
-     var oHeaders =  {
-        "Content-Type": sContentType
-     };
-
-     oResponse.writeHead(200, oHeaders);
-     var oFileStream = oFs.createReadStream(sFSPath);
-     oFileStream.pipe(oResponse);
-     oFileStream.on('error',function(e) {
-         // assumes the file doesn't exist
-         oResponse.writeHead(404);
-         oResponse.end()
-     });
-   } catch(e) {
+    oResponse.writeHead(200, oHeaders);
+    var oFileStream = oFs.createReadStream(sFSPath);
+    oFileStream.pipe(oResponse);
+    oFileStream.on('error' ,function(e) {
+        // assumes the file doesn't exist
+        oResponse.writeHead(404);
+        oResponse.end()
+    });
+  } catch(e) {
      oResponse.writeHead(500);
 
      // ends the oResponse so browsers don't hang
      oResponse.end();
      console.log(e.stack)
-   }
+  }
 }).listen(port);
 
 console.log("listening on port \"" + port + "\"");
