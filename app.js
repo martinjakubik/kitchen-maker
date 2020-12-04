b126beccb342be86a78cb5ffc1c7c7f7dc68b5da
martin jakubik <marcin.jakubik@sap.com>
2020-12-04 18:21:25 +0100

    put text on web page

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('let\'s make some kitchens!');
+let sMessage = 'let\'s make some kitchens!';
+console.log(sMessage);
+
+let contentParagraph = document.createElement('p');
+let contentText = document.createTextNode(sMessage);
+contentParagraph.appendChild(contentText);
+document.body.appendChild(contentParagraph);
