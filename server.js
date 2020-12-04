8bce3b04a8d7e573b2f24da9eb03d1cfdf77fdbc
martin jakubik <marcin.jakubik@sap.com>
2020-12-04 16:08:24 +0100

    tabs = 4

--- server.js
+++ server.js
@@ -7,57 +7,57 @@ let sBaseDirectory = ".";
 let nPort = 8080;
 
 oHttp.createServer(function (oRequest, oResponse) {
-  try {
-    let oRequestUrl = oUrl.parse(oRequest.url);
+    try {
+        let oRequestUrl = oUrl.parse(oRequest.url);
 
-    let sPath = oRequestUrl.pathname;
+        let sPath = oRequestUrl.pathname;
 
-    // need to use oPath.normalize so people can't access directories underneath sBaseDirectory
-    let sFSPath = sBaseDirectory + oPath.normalize(sPath);
-    console.log("normalized path: \"" + sFSPath + "\"");
+        // need to use oPath.normalize so people can't access directories underneath sBaseDirectory
+        let sFSPath = sBaseDirectory + oPath.normalize(sPath);
+        console.log("normalized path: \"" + sFSPath + "\"");
 
-    if (process.platform === 'win32') {
-      console.log('we\'re on windows');
-      if (sFSPath === '.\\') {
-        sFSPath = '.\\index.html';
-      }
-    } else {
-      if (sFSPath === './') {
-        sFSPath = './index.html';
-      }
+        if (process.platform === 'win32') {
+            console.log('we\'re on windows');
+            if (sFSPath === '.\\') {
+                sFSPath = '.\\index.html';
+            }
+        } else {
+            if (sFSPath === './') {
+                sFSPath = './index.html';
+            }
+        }
+
+        console.log("transformed path: \"" + sFSPath + "\"");
+
+        let sContentType = "text/plain";
+
+        if (sFSPath.includes("/css/")) {
+            sContentType = "text/css";
+        } else if (sFSPath.includes("/html/")) {
+            sContentType = "text/html";
+        } else if (sFSPath.includes("/js/")) {
+            sContentType = "application/javascript";
+        }
+
+        let oHeaders =  {
+           "Content-Type": sContentType
+        };
+
+        oResponse.writeHead(200, oHeaders);
+        let oFileStream = oFs.createReadStream(sFSPath);
+        oFileStream.pipe(oResponse);
+        oFileStream.on('error' ,function(e) {
+            // assumes the file doesn't exist
+            oResponse.writeHead(404);
+            oResponse.end()
+        });
+    } catch(e) {
+        oResponse.writeHead(500);
+
+        // ends the oResponse so browsers don't hang
+        oResponse.end();
+        console.log(e.stack)
     }
-
-    console.log("transformed path: \"" + sFSPath + "\"");
-
-    let sContentType = "text/plain";
-
-    if (sFSPath.includes("/css/")) {
-        sContentType = "text/css";
-    } else if (sFSPath.includes("/html/")) {
-        sContentType = "text/html";
-    } else if (sFSPath.includes("/js/")) {
-        sContentType = "application/javascript";
-    }
-
-    let oHeaders =  {
-       "Content-Type": sContentType
-    };
-
-    oResponse.writeHead(200, oHeaders);
-    let oFileStream = oFs.createReadStream(sFSPath);
-    oFileStream.pipe(oResponse);
-    oFileStream.on('error' ,function(e) {
-        // assumes the file doesn't exist
-        oResponse.writeHead(404);
-        oResponse.end()
-    });
-  } catch(e) {
-     oResponse.writeHead(500);
-
-     // ends the oResponse so browsers don't hang
-     oResponse.end();
-     console.log(e.stack)
-  }
 }).listen(nPort);
 
 console.log("listening on nPort \"" + nPort + "\"");
